fa7960ff8c530bc474b2b6799ec6cdcf8bbcbf28
Ofek Nar <ofek2608@gmail.com>
2022-05-17 04:40:34 +0300

    Rename PlayerNew to Player

--- src/main/java/com/ofek2608/dnd/api/player/Player.java
+++ src/main/java/com/ofek2608/dnd/api/player/Player.java
@@ -1,14 +1,10 @@
 package com.ofek2608.dnd.api.player;
 
-import com.ofek2608.dnd.api.Player;
 import com.ofek2608.dnd.api.Savable;
-import com.ofek2608.dnd.impl.PlayerImpl;
+import com.ofek2608.dnd.impl.player.PlayerImpl;
 import net.dv8tion.jda.api.entities.User;
 
-import javax.annotation.Nullable;
-
-public interface PlayerNew extends Savable {
-
+public interface Player extends Savable {
 	static Player getPlayer(User user) {
 		return PlayerImpl.getPlayer(user);
 	}
@@ -20,8 +16,10 @@ public interface PlayerNew extends Savable {
 	PlayerStats getStats();
 
 
+	void openMenu();
+	void openAdventure();
+	void openInventory();
 	void continueAdventure();
 	void goHome();
 	void respawn();
-	void openInventory();
 }
